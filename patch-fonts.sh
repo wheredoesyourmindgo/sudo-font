fc67fbc7bf3cdd558136486564a8910fcbb8158b
wheredoesyourmindgo <moemanchu@icloud.com>
2020-03-15 17:54:58 -0700

    patch-fonts.sh updated.

--- patch-fonts.sh
+++ patch-fonts.sh
@@ -5,6 +5,7 @@ DIST_DIR=dist
 IN_FONT_DIR=sudo
 OUT_FONT_DIR=sudo/patch
 TTX_DIR=ttf_ttx/patch
+FONT_PATCH_OPT="--octicons --powerline --powerlineextra"
 
 ttx -f -d $TTX_DIR $IN_FONT_DIR/Sudo-Regular.ttf
 ttx -f -d $TTX_DIR $IN_FONT_DIR/Sudo-Italic.ttf
@@ -31,16 +32,16 @@ ttx -f -d $OUT_FONT_DIR $TTX_DIR/Sudo-Thin.ttx
 ttx -f -d $OUT_FONT_DIR $TTX_DIR/Sudo-ThinItalic.ttx
 
 if command -v font-patcher 1>/dev/null 2>&1; then
-  font-patcher --octicons --powerline --powerlineextra -out $OUT_FONT_DIR $OUT_FONT_DIR/Sudo-Bold.ttf
-  font-patcher --octicons --powerline --powerlineextra -out $OUT_FONT_DIR $OUT_FONT_DIR/Sudo-BoldItalic.ttf
-  font-patcher --octicons --powerline --powerlineextra -out $OUT_FONT_DIR $OUT_FONT_DIR/Sudo-Light.ttf
-  font-patcher --octicons --powerline --powerlineextra -out $OUT_FONT_DIR $OUT_FONT_DIR/Sudo-LightItalic.ttf
-  font-patcher --octicons --powerline --powerlineextra -out $OUT_FONT_DIR $OUT_FONT_DIR/Sudo-Regular.ttf
-  font-patcher --octicons --powerline --powerlineextra -out $OUT_FONT_DIR $OUT_FONT_DIR/Sudo-Italic.ttf
-  font-patcher --octicons --powerline --powerlineextra -out $OUT_FONT_DIR $OUT_FONT_DIR/Sudo-Thin.ttf
-  font-patcher --octicons --powerline --powerlineextra -out $OUT_FONT_DIR $OUT_FONT_DIR/Sudo-ThinItalic.ttf
-  font-patcher --octicons --powerline --powerlineextra -out $OUT_FONT_DIR $OUT_FONT_DIR/Sudo-Medium.ttf
-  font-patcher --octicons --powerline --powerlineextra -out $OUT_FONT_DIR $OUT_FONT_DIR/Sudo-MediumItalic.ttf
+  eval font-patcher $FONT_PATCH_OPT -out $OUT_FONT_DIR $OUT_FONT_DIR/Sudo-Bold.ttf
+  eval font-patcher $FONT_PATCH_OPT -out $OUT_FONT_DIR $OUT_FONT_DIR/Sudo-BoldItalic.ttf
+  eval font-patcher $FONT_PATCH_OPT -out $OUT_FONT_DIR $OUT_FONT_DIR/Sudo-Light.ttf
+  eval font-patcher $FONT_PATCH_OPT -out $OUT_FONT_DIR $OUT_FONT_DIR/Sudo-LightItalic.ttf
+  eval font-patcher $FONT_PATCH_OPT -out $OUT_FONT_DIR $OUT_FONT_DIR/Sudo-Regular.tteval f
+  eval font-patcher $FONT_PATCH_OPT -out $OUT_FONT_DIR $OUT_FONT_DIR/Sudo-Italic.ttf
+  eval font-patcher $FONT_PATCH_OPT -out $OUT_FONT_DIR $OUT_FONT_DIR/Sudo-Thin.ttf
+  eval font-patcher $FONT_PATCH_OPT -out $OUT_FONT_DIR $OUT_FONT_DIR/Sudo-ThinItalic.ttf
+  eval font-patcher $FONT_PATCH_OPT -out $OUT_FONT_DIR $OUT_FONT_DIR/Sudo-Medium.ttf
+  eval font-patcher $FONT_PATCH_OPT -out $OUT_FONT_DIR $OUT_FONT_DIR/Sudo-MediumItalic.ttf
 fi
 
 rm -f $DIST_DIR/$ARCHIVE
